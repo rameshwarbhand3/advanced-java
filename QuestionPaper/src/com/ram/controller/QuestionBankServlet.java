c74470556e3ee414664f9885a8d112a5e40313e1
rameshwarbhand3 <rameshwarbhand3@gmail.com>
2023-07-05 11:24:43 +0530

    remove unused imports

--- QuestionPaper/src/com/ram/controller/QuestionBankServlet.java
+++ QuestionPaper/src/com/ram/controller/QuestionBankServlet.java
@@ -1,7 +1,6 @@
 package com.ram.controller;
 
 import java.io.IOException;
-import java.util.Date;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -11,7 +10,6 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.ram.dao.QuestionDao;
 import com.ram.model.Category;
